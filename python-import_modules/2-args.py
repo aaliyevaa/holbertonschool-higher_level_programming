321854611673481315f1e0280df9c89b62d5fd64
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-16 22:29:23 +0400

    FIx the script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+count = len(sys.argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:\n1: {}".format(sys.argv[1]))
+else:
+    print("{} arguments:\n".format(count))
+    i = 1
+    while i <= count:
+        print("{}: {}".format(i,sys.argv[i]))
+        i += 1
