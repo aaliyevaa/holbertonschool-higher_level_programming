6496f9996ea93ed9fdf4f3d5c36ffb1bbcaf066a
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-16 22:32:32 +0400

    Add if statement

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:\n1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:\n".format(count))
-    i = 1
-    while i <= count:
-        print("{}: {}".format(i,sys.argv[i]))
-        i += 1
+if __name__ == '__main__':
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:\n".format(count))
+        i = 1
+        while i <= count:
+            print("{}: {}".format(i,sys.argv[i]))
+            i += 1
