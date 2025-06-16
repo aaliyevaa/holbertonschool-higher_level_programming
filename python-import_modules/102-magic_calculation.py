40655f2170a80cfc9ab35d1674b2884731ad34c3
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-17 00:58:04 +0400

    Pycodestyle edit

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import add,sub from magic_calculation_102
-def magic_calculation(a,b):
+
+from magic_calculation_102 import add, sub
+
+
+def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
-    for i in range(4, 6):
-        c = add(i, c)
-    return c
+        for i in range(4, 6):
+            c = add(i, c)
+        return c
     else:
         return sub(a, b)
