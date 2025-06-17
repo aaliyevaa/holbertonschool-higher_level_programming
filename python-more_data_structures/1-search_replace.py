3743b15a87b5e6365aa5c8c3365fa1cf822b3f01
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-17 21:44:52 +0400

    Fix unmatching parantheses

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new = [i if i != search else replace for i in my_list)
+    new = [replace if i == search else i for i in my_list]
     return new
