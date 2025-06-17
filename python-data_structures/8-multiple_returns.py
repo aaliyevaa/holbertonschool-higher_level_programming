80533c22a5d1995eb560f3a8629ed43b1916326e
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-17 16:39:46 +0400

    Length issue

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
+    length = len(sentence)
+    if length == 0:
         first = None
     else:
-        length = len(sentence)
         first = sentence[0]
     return (length, first)
