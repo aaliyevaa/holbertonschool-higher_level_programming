45ad216d0d47c6e0be1810e6becc16095a27fbc1
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-17 22:57:50 +0400

    Change return value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,8 +3,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     keys = [key for key in a_dictionary.keys()]
-    if len(keys) == 1:
-        return None
     maxx = a_dictionary[keys[0]]
     max_key = keys[0]
     for keys in a_dictionary:
