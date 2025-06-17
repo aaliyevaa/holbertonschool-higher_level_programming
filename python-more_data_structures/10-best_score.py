6ce8e2c362afcdbf3b3f2e8f592a4bb81e25beee
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-17 22:53:07 +0400

    Return none when not a dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     keys = [key for key in a_dictionary.keys()]
+    if len(keys) == 1:
+        return None
     maxx = a_dictionary[keys[0]]
     for keys in a_dictionary:
         if a_dictionary[keys] > maxx:
