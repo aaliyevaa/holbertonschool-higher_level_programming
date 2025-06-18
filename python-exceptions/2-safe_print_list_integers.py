d4fb2ae5c874377a16c8ab8b668a18ca3e01f5e3
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-18 14:40:47 +0400

    Use format string

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             try:
                 count += 1
-                print(my_list[i], end='')
+                print("{:d}".format(my_list[i]), end='')
             except (ValueError, TypeError):
                  pass
     except IndexError:
