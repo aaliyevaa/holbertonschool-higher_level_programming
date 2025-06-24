8856643ec47bf0198bcbc53c8be7819338f9005e
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-24 17:14:30 +0400

    Make variables private

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+""" This module has a class Rectangle."""
+
+
+class Rectangle:
+    """ This class defines a rectangle by its width and height."""
+    def __init__(self, width=0, height=0):
+        """ Initiation with width and height"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """ Get the value of height"""
+        return self.__height
+
+    @property
+    def width(self):
+        """ Get the value of width"""
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """ Set the value of heigth with validation."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """ Set the value of width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value            
