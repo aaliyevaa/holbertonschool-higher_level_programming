0ab07ebc1a9503288d114978dc54c902b8fee035
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-24 20:15:55 +0400

    fix typo

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """ Initialize with width and height."""
         self.width = width
-        slef.height = height
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
