c1c7e557110f905329f9eafe479572f19cda17b8
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-25 20:10:44 +0400

    Execute permission to all

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module has a class inheriting from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """This is a class."""
+    def __init__(self, width, height):
+        """Initialize with height and width"""
+        self.integer_validator("widht", width)
+        self.integer_validator("height", height)
+        self.__widht = width
+        self.__height = height
