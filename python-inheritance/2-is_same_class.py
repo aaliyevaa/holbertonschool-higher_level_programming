5b2380ffcd4ef56865492b7211f8e44f9a0873f8
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-25 18:18:50 +0400

    Fix

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,6 +2,4 @@
 """This module has a function."""
 def is_same_class(obj, a_class):
     """Checks if an objects is an instance of a specified class"""
-    if isinstance(obj, a_class):
-        return True
-    return False
+    return type(obj) is a_class
