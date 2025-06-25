22e12b323da4cb678bb0785bb9f4b645f9463547
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-25 18:47:28 +0400

    Fix code

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Checks is obj inherits from a_class."""
-    return issubclass(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
