ebc18f90f8e749b2b1990a6e2c4793074764cffe
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-25 18:20:30 +0400

    Pycodestyle edit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module has a function."""
+
+
 def is_same_class(obj, a_class):
     """Checks if an objects is an instance of a specified class"""
     return type(obj) is a_class
