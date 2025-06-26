976a58639afd40aa0f3bec9610c26748beac4de7
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 20:47:36 +0400

    Fix typo

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module has a class Student."""
+
+
+class Student:
+    """This class defines a student by first_name, last_name
+    and age"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize with first_name,last_name, age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary"""
+        if isinstance(attrs, list) and all(
+            isinstance(attr, str) for attr in attrs
+        ):
+            return {
+                key: getattr(self, key)
+                for key in attrs if hasattr(self, key)
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces"""
+        for key, value in json.getitems():
+            setattr(self, key, value)
