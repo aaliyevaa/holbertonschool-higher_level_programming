e22558e91c44a0f9bbcec466b9244ed8885e51b0
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 18:40:20 +0400

    For loop

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """This function opens a file and reads it."""
     with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end=' ')
