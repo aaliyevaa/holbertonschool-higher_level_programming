2687a7ae4984a4d35774e765c3aac30ca39d5340
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 21:45:15 +0400

    Open read and write separately

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a module"""
+
+
+def append_after(filename='', search_string='', new_string=''):
+    new_lines = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            new_lines += [line]
+            if search_string in line:
+                new_lines += [new_string]
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(new_lines)
