1e649fb00e12d6b6fd3b8f0bafd8fdf007bb2363
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 19:40:00 +0400

    check if file exists

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,5 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """This function writes <my_obj> to the file <filename>"""
     with open(filename, 'w', encoding='utf-8') as f:
-        json_string = json.dumps(my_obj)
-        f.write(json_string)
+        json.dump(my_obj, f)
