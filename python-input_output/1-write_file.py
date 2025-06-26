988301422c95a6309b0549827a4eca96abfea7e8
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 18:48:13 +0400

    Pycodestyle edit

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename='', text=''):
+    """This function writes <text> inside the file <filename>"""
     with open(filename, 'w', encoding='utf-8') as f:
-       return(f.write(text))
+        return (f.write(text))
