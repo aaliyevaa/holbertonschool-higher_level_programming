a9e5a23d88422fe2da54a7ef1aa6fb3a8727d7a0
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 18:58:59 +0400

    Execute permission to all

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module has a function."""
+import json
+
+
+def to_json_string(my_obj):
+    """This function returns the json string of <my_obj>"""
+    return json.dumps(my_obj)
