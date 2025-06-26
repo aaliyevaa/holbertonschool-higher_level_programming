551fe45e527d856a3e7c052dafd8868918c42568
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 20:44:08 +0400

    PEP8

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,6 +13,11 @@ class Student:
 
     def to_json(self, attrs=None):
         """Retrieves a dictionary"""
-        if not attrs:
-            return dict(self.__dict__)
-        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        if isinstance(attrs, list) and all(
+            isinstance(attr, str) for attr in attrs
+        ):
+            return {
+                key: getattr(self, key)
+                for key in attrs if hasattr(self, key)
+            }
+        return self.__dict__
