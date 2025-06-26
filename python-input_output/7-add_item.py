f69efdd451ce0981178c4cb496089252eb8d58b9
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-26 19:41:11 +0400

    check if file exists

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,7 @@
 """Import sys and json libraries to use their built-in functions."""
 import sys
 import json
-from pathlib import Path
+import os
 
 """Import 2 previous functions from previous files."""
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
@@ -12,7 +12,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 """Check if the file exists. If not, create a new one"""
 filename = 'add_itme.json'
-if Path(filename).exists():
+if os.path.exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
