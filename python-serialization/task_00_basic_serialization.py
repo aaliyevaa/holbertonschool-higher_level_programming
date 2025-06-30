f7d11be9d60965cebf2d32cc667eb4c5477aeea9
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-30 23:00:44 +0400

    PEP8

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-"""This module has 2 functions for serialization and 
+"""This module has 2 functions for serialization and
 deserialization"""
 import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """This function serializes the <data> to JSON string and 
+    """This function serializes the <data> to JSON string and
     saves it in a file called <filename>"""
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
+
 
 def load_and_deserialize(filename):
     """This function loads the file <filename> and deserializes
     it"""
     with open(filename, 'r', encoding='utf-8') as f:
-        json.load(f)
+        return json.load(f)
