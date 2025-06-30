4300cda746b3a644560646a96f184d8915db35ba
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-30 23:42:45 +0400

    Create a data list

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,11 +8,14 @@ def convert_csv_to_json(csv_filename):
     """Convert CSV file to JSON format. Return True if
     successful"""
     try:
-        with open(csv_filename, mode='r', newline='',
-                  encoding='utf-8') as csv_file:
+        with open(csv_filename, mode='r', encoding='utf-8')
+        as csv_file:
             data = list(csv.DictReader(csv_file))
+            data_list = []
+            for i in data:
+                data_list.append(i)
         with open('data.json', mode='w', encoding='utf-8') as jfile:
-            json.dump(data, jfile, indent=4)
+            json.dump(data_list, jfile, indent=4)
 
         return:
             True
