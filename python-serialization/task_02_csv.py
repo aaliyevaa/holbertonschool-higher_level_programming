a61a1f1137d9c40dc62ce74b0bad5267398e3331
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-30 23:47:04 +0400

    Fix typos

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -14,7 +14,7 @@ def convert_csv_to_json(filename):
             for i in data:
                 data_list.append(i)
         with open('data.json', mode='w', encoding='utf-8') as file:
-            json.dump(data_list, fp=file)
+            json.dump(data_list, file)
         return True
     except Exception:
         return False
