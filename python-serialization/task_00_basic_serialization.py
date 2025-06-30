b00ef8098521d6e544aa52180cc3a3a1e0d84d3f
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-30 22:55:00 +0400

    Initial commit

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module has 2 functions for serialization and 
+deserialization"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """This function serializes the <data> to JSON string and 
+    saves it in a file called <filename>"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+def load_and_deserialize(filename):
+    """This function loads the file <filename> and deserializes
+    it"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        json.load(f)
