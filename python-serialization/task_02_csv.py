4fdffe04ef21c51b4c08e53e619c44cea940a776
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-06-30 23:53:23 +0400

    PEP8

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,13 +8,12 @@ def convert_csv_to_json(filename):
     """Convert CSV file to JSON format. Return True if
     successful"""
     try:
-        with open(filename, mode='r', encoding='utf-8' as file:
+        with open(filename, mode='r', encoding='utf-8') as file:
             data = csv.DictReader(file)
-            data_list = []
-            for i in data:
-                data_list.append(i)
-        with open('data.json', mode='w', encoding='utf-8') as file:
-            json.dump(data_list, file)
+            data_list = [row for row in data]
+
+        with open('data.json', mode='w', encoding='utf-8') as j:
+            json.dump(data_list, j, indent=4)
         return True
     except Exception:
         return False
