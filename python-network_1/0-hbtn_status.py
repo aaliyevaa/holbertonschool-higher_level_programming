496ed339de7b39d07a3e0687d4f9bc55dc29e4d6
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-07-09 18:44:47 +0400

    Fix typos

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetches a URL"""
 from urllib import request
 
-if __name__ == "__main__":
+if ___name__ == "__main__":
     """Makes code executable when it is directly run"""
     url = "https://intranet.hbtn.io/status"
     req = request.Request(url)
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         utf8_content = body.decode('utf-8')
 
     """Prints out the desired output"""
-    print("Body responde:")
+    print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
     print("\t- utf8 content:", utf8_content)
