0b25ebeb31b443c6e7ff9365e1498fcbaa8732d8
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-07-09 19:00:02 +0400

    Get header

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Take in a URL, send a request and display
+the value of the X-Request-Id"""
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    """Ensure code runs when directly run"""
+    url = sys.args[1]
+    req = request.Request(url)
+    req.add_header("cfclearance", "true")
+
+    with request.urlopen(req) as response:
+        """Access the header"""
+        header = response.getheader("X-Request-Id")
+
+    print(header)
