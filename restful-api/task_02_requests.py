e3c13deea85db2f45c63b137e8a72a68c7d8bf20
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-07-14 18:40:17 +0400

    Initial commit with the task

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Ftech posts from JSONplaceholder"""
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """ Fetch and print titles of all posts"""
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    print(f"Status code: {response.status_code}")
+    if response.status_code == 200:
+        data = respone.json()
+        for post in data:
+            print(post['title'])
+    else:
+        print("Failed to fetch posts")
+
+    
+def fetch_and_save_posts():
+    """Fetch and save posts and save them to CSV file"""
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in data]
+        with open('posts.csv.', mode='w', newline='', encoding='utf-8') as file:
+            fieldnames = ['id','title','body']
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(post_data)
+
+        print("Posts saved to posts.csv")
+    else:
+        print("Failed to fetch posts")
+
+if __name__ == "__main__":
+    fetch_and_print_posts()
+    fetch_and_save_posts()
+
