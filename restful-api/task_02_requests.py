2a168718c487c68b2ec4d3589280e80cf6b31deb
aaliyevaa <amalyaaliyeva74@gmail.com>
2025-07-14 18:43:35 +0400

    Fix typos

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,7 +8,7 @@ def fetch_and_print_posts():
     """ Fetch and print titles of all posts"""
     url = 'https://jsonplaceholder.typicode.com/posts'
     response = requests.get(url)
-    print(f"Status code: {response.status_code}")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         data = response.json()
         for post in data:
